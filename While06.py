e0f5070112a2bc05bd60866a2f88bcafbdc2754c
ja <jaxa42421@gmail.com>
2025-07-01 12:33:19 +0500

    task6

--- While06.py
+++ While06.py
@@ -1,9 +1,18 @@
 def main(s):
+    a = 0
+    i = 0
+    vowels = "aeiouAEIOU"
+    while i < len(s):
+        if s[i].isalpha() and s[i] not in vowels:
+            a += 1
+        i += 1
     """
-    A variable of type str is given. Find and return how many consonant letters there are.
+    A variable of type str is given. Find how many consonant letters there are and return.
     Args:
         s: str
-        consonant: other than vowels(a, e, i, o, u)
     Returns:
         int: return answer
     """
+    return a
+
+print(main("python %^&^%$ 2022"))  # Output: 5 (p, y, t, h, n)
