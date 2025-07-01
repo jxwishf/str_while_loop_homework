d7d0f1c6f549dddfb26ff52711c95d1c5e876e98
ja <jaxa42421@gmail.com>
2025-07-01 12:36:50 +0500

    task10

--- While10.py
+++ While10.py
@@ -1,4 +1,10 @@
 def main(s):
+    a = 0
+    i = 0
+    while i < len(s):
+        if s[i].isdigit() and int(s[i]) % 2 == 1:
+            a += int(s[i])
+        i += 1
     """
     A string of numbers is given. Find and return the sum of all odd digits.
     Args:
@@ -6,4 +12,6 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    return a
+
+print(main("123abc456")) 
